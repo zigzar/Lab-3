e2f23bb677805a023339979199c17592d4303868
zigzar <zigzariz@mail.ru>
2019-11-25 01:48:38 +0300

    Added spiral (need to fix links)

--- Lab-3/Lab-3/Lab-3.cpp
+++ Lab-3/Lab-3/Lab-3.cpp
@@ -2,17 +2,11 @@
 
 using namespace std;
 
-const int N = 7;
 
-/*void fillSpiral(*arr, ) {
+template <size_t N>
 
-};*/
-
-int main()
-{
-	setlocale(0, "");
-	short A[N][N];
-	for (int j = 0, count = 1; j < N;)
+void fillSnake(int (&A)[N][N], int count) {
+	for (int j = 0; j < N;)
 	{
 		for (int i = 0; i < N; i++)
 		{
@@ -20,20 +14,102 @@ int main()
 		}
 		j++;
 		if (j == N) break;
-		for (int i = N-1; i >= 0; i--)
+		for (int i = N - 1; i >= 0; i--)
 		{
 			A[i][j] = count++;
 		}
 		j++;
 		if (j == N) break;
 	};
+};
 
-	cout << "Заполнение змейкой:\n";
+template <size_t N>
+void fillRight(int (&A)[N][N], int length, int &curRow, int &curCol, int &count) {
+	while (curCol <= length) {
+		A[curRow][curCol] = count++;
+		curCol++;
+	}
+	curCol--;
+	curRow++;
+}
+
+template <size_t N>
+void fillDown(int(&A)[N][N], int length, int &curRow, int &curCol, int &count) {
+	while (curRow <= length) {
+		A[curRow][curCol] = count++;
+		curRow++;
+	}
+	curCol--;
+	curRow--;
+}
+
+template <size_t N>
+void fillLeft(int(&A)[N][N], int length, int &curRow, int &curCol, int &count) {
+	while (curCol <= length) {
+		A[curRow][curCol] = count++;
+		curCol--;
+	}
+	curCol++;
+	curRow--;
+}
+
+template <size_t N>
+void fillUp(int(&A)[N][N], int length, int &curRow, int &curCol, int &count) {
+	while (curRow <= length) {
+		A[curRow][curCol] = count++;
+		curRow--;
+	}
+	curCol++;
+	curRow++;
+}
+
+template <size_t N>
+void fillDirectional(int (&A)[N][N],int length, int &curRow, int &curCol, int &direction, int &count) {
+	switch (direction%4)
+	{
+	case 0:
+		fillRight(A, length, curRow, curCol, count);
+		break;
+	case 1:
+		fillDown(A, length, curRow, curCol, count);
+		break;
+	case 2:
+		fillLeft(A, length, curRow, curCol, count);
+		break;
+	case 3:
+		fillUp(A, length, curRow, curCol, count);
+		break;
+	}
+	direction++;
+}
+
+template <size_t N>
+void fillSpiral(int(&A)[N][N], int count) {
+	int direction, curRow, curCol = 0;
+	for (int length = N; length > 0; length--)
+	{
+		if (length == N) {
+			fillDirectional(A, length, curRow, curCol, direction, count);
+		} else if (length > 0) {
+			for (int i = 0; i < 2; i++) {
+				fillDirectional(A, length, curRow, curCol, direction, count);
+			}
+		}
+	}
+}
+
+int main()
+{
+	setlocale(0, "");
+	const int N = 7;
+	int matrix[N][N] = { 0 };
+	//fillSnake(matrix, 1);
+	fillSpiral(matrix, 1);
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			cout << A[i][j] << "\t";
+			cout << matrix[i][j] << "\t";
 			if (j == N-1)
 			{
 				cout << endl;
