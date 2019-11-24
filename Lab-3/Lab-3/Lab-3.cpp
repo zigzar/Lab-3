373dde2558ac08482a31c69155c194e8fad7847d
Максим Голяков <zigzariz@mail.ru>
2019-11-24 20:00:42 +0300

    Added snake filling

---
+++ Lab-3/Lab-3/Lab-3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int N = 7;
+
+/*void fillSpiral(*arr, ) {
+
+};*/
+
+int main()
+{
+	setlocale(0, "");
+	short A[N][N];
+	for (int j = 0, count = 1; j < N;)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			A[i][j] = count++;
+		}
+		j++;
+		if (j == N) break;
+		for (int i = N-1; i >= 0; i--)
+		{
+			A[i][j] = count++;
+		}
+		j++;
+		if (j == N) break;
+	};
+
+	cout << "Заполнение змейкой:\n";
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << A[i][j] << "\t";
+			if (j == N-1)
+			{
+				cout << endl;
+			}
+		}
+	}
+
+	short B[N][N];
+
+}
