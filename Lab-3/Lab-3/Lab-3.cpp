acc1d1bc6fc1af0ade88863113e58f6238d456e8
Максим Голяков <zigzariz@mail.ru>
2019-12-03 21:46:53 +0300

    Dynamic arrays

--- Lab-3/Lab-3/Lab-3.cpp
+++ Lab-3/Lab-3/Lab-3.cpp
@@ -1,13 +1,78 @@
 ﻿#include <iostream>
 #include <string>
 #include <fstream>
+#include <vector> 
 
 using namespace std;
 
 ofstream fout;
 
-template <size_t N>
-void coutMatrix(int(&A)[N][N]) {
+void getRank(int** A, int N) {
+	int rank = N;
+	vector<char> line_used(N);
+	for (int i = 0; i < N; ++i) {
+		int j;
+		for (j = 0; j < N; ++j)
+			if (!line_used[j] && A[j][i] != 0)
+				break;
+		if (j == N)
+			--rank;
+		else {
+			line_used[j] = true;
+			for (int p = i + 1; p < N; ++p)
+				A[j][p] /= A[j][i];
+			for (int k = 0; k < N; ++k)
+				if (k != j && A[j][i] != 0)
+					for (int p = i + 1; p < N; ++p)
+						A[k][p] -= A[j][p] * A[k][i];
+		}
+	}
+	cout << endl << "Ранг матрицы: " << rank << endl << endl;
+}
+
+void getMatr(int** mas, int** p, int i, int j, int m)
+{
+	int ki, kj, di, dj;
+	di = 0;
+	for (ki = 0; ki < m - 1; ki++) { // проверка индекса строки
+		if (ki == i) di = 1;
+		dj = 0;
+		for (kj = 0; kj < m - 1; kj++) { // проверка индекса столбца
+			if (kj == j) dj = 1;
+			p[ki][kj] = mas[ki + di][kj + dj];
+		}
+	}
+}
+int getDet(int** mas, int m) {
+	int i, j, d, k, n;
+	int** p = new int* [m];
+	for (int i = 0; i < m; i++)
+		p[i] = new int[m];
+	j = 0; d = 0;
+	k = 1;
+	n = m - 1;
+	switch (m)
+	{
+	case 1:
+		d = mas[0][0];
+		return(d);
+		break;
+	case 2:
+		d = mas[0][0] * mas[1][1] - mas[1][0] * mas[0][1];
+		return(d);
+		break;
+	default:
+		for (i = 0, j = 0; j < m; j++) {
+			getMatr(mas, p, 0, j, m);
+			d = d + k * mas[0][j] * getDet(p, n);
+			k = -k;
+		}
+		return(d);
+		break;
+	}
+}
+
+void coutMatrix(int** A, int N) {
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
@@ -18,8 +83,7 @@ void coutMatrix(int(&A)[N][N]) {
 	}
 }
 
-template <size_t N>
-void foutMatrix(int(&A)[N][N]) {
+void foutMatrix(int** A, int N) {
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
@@ -30,15 +94,14 @@ void foutMatrix(int(&A)[N][N]) {
 	}
 }
 
-template <size_t N>
-void fillSnake(int (&A)[N][N], int count) {
+void fillSnake(int** A, int N, int count) {
 	for (int j = 0; j < N;)
 	{
 		for (int i = 0; i < N; i++)
 		{
 			A[i][j] = count++;
 			system("cls");
-			coutMatrix(A);
+			coutMatrix(A, N);
 		}
 		j++;
 		if (j == N) break;
@@ -46,79 +109,73 @@ void fillSnake(int (&A)[N][N], int count) {
 		{
 			A[i][j] = count++;
 			system("cls");
-			coutMatrix(A);
+			coutMatrix(A, N);
 		}
 		j++;
 		if (j == N) break;
 	};
 };
 
-template <size_t N>
-void fillRight(int (&A)[N][N], int length, int &curRow, int &curCol, int &count) {
+void fillRight(int** A, int N, int length, int &curRow, int &curCol, int &count) {
 	for (int i = 0; i < length; i++, curCol++) {
 		A[curRow][curCol] = count++;
 		system("cls");
-		coutMatrix(A);
+		coutMatrix(A, N);
 	}
 	curCol--;
 	curRow++;
 }
 
-template <size_t N>
-void fillDown(int(&A)[N][N], int length, int &curRow, int &curCol, int &count) {
+void fillDown(int **A, int N, int length, int &curRow, int &curCol, int &count) {
 	for (int i = 0; i < length; i++, curRow++) {
 		A[curRow][curCol] = count++;
 		system("cls");
-		coutMatrix(A);
+		coutMatrix(A, N);
 	}
 	curCol--;
 	curRow--;
 }
 
-template <size_t N>
-void fillLeft(int(&A)[N][N], int length, int &curRow, int &curCol, int &count) {
+void fillLeft(int** A, int N, int length, int &curRow, int &curCol, int &count) {
 	for (int i = 0; i < length; i++, curCol--) {
 		A[curRow][curCol] = count++;
 		system("cls");
-		coutMatrix(A);
+		coutMatrix(A, N);
 	}
 	curCol++;
 	curRow--;
 }
 
-template <size_t N>
-void fillUp(int(&A)[N][N], int length, int &curRow, int &curCol, int &count) {
+void fillUp(int** A, int N, int length, int &curRow, int &curCol, int &count) {
 	for (int i = 0; i < length; i++, curRow--) {
 		A[curRow][curCol] = count++;
 		system("cls");
-		coutMatrix(A);
+		coutMatrix(A, N);
 	}
 	curCol++;
 	curRow++;
 }
 
-template <size_t N>
-void fillDirectional(int (&A)[N][N],int length, int &curRow, int &curCol, int &direction, int &count) {
+void fillDirectional(int** A, int N, int length, int &curRow, int &curCol, int &direction, int &count) {
 	switch (direction%4)
 	{
 	case 0:
-		fillRight(A, length, curRow, curCol, count);
+		fillRight(A, N, length, curRow, curCol, count);
 		break;
 	case 1:
-		fillDown(A, length, curRow, curCol, count);
+		fillDown(A, N, length, curRow, curCol, count);
 		break;
 	case 2:
-		fillLeft(A, length, curRow, curCol, count);
+		fillLeft(A, N, length, curRow, curCol, count);
 		break;
 	case 3:
-		fillUp(A, length, curRow, curCol, count);
+		fillUp(A, N, length, curRow, curCol, count);
 		break;
 	}
 	direction++;
 }
 
-template <size_t N>
-void fillSpiral(int(&A)[N][N], int start) {
+void fillSpiral(int** A, int N, int start) {
 	int count = start;
 	int direction = 0;
 	int curRow = 0;
@@ -126,10 +183,10 @@ void fillSpiral(int(&A)[N][N], int start) {
 	for (int length = N; length > 0; length--)
 	{
 		if (length == N) {
-			fillDirectional(A, length, curRow, curCol, direction, count);
+			fillDirectional(A, N, length, curRow, curCol, direction, count);
 		} else if (length > 0) {
 			for (int i = 0; i < 2; i++) {
-				fillDirectional(A, length, curRow, curCol, direction, count);
+				fillDirectional(A, N, length, curRow, curCol, direction, count);
 			}
 		}
 	}
@@ -141,12 +198,23 @@ int main()
 
 
 	string path = "matrix.txt";
-	int Order = 11;
 
-	int **matrix = int* [Order];
+	cout << "Введите порядок квадратной матрицы:" << endl;
+	int Order;
+	cin >> Order;
+
+	int **matrix = new int* [Order]; // Создание матрицы
 	for (int i = 0; i < Order; i++)
 	{
-		matrix[i] = int[Order];
+		matrix[i] = new int [Order];
+	}
+
+	for (int i = 0; i < Order; i++) // Инициализация матрицы
+	{
+		for (int j = 0; j < Order; j++)
+		{
+			matrix[i][j] = 0;
+		}
 	}
 	
 	int method, start;
@@ -164,11 +232,11 @@ int main()
 		switch (method)
 		{
 		case 1:
-			fillSnake(matrix, start);
+			fillSnake(matrix, Order, start);
 			goto inpSucc;
 			break;
 		case 2:
-			fillSpiral(matrix, start);
+			fillSpiral(matrix, Order, start);
 			goto inpSucc;
 			break;
 		default:
@@ -181,7 +249,7 @@ int main()
 	fout.open(path);
 	try
 	{
-		foutMatrix(matrix);
+		foutMatrix(matrix, Order);
 	}
 	catch (const exception&)
 	{
@@ -194,4 +262,6 @@ int main()
 		delete [] matrix[i];
 	}
 	delete[] matrix;
+
+	system("pause");
 }
